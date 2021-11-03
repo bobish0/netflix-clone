d211d13b8adad172e7fd205203f363be35978d1e
bobish0 <kristian.a.boban@gmail.com>
2021-11-03 01:01:46 +0100

    added navbar-selections, no ul/li that is normally best practice, but for this mockup it will suffice

--- src/Nav.js
+++ src/Nav.js
@@ -16,7 +16,7 @@ function Nav() {
             } else handleShow(false);
         });
         return () => { // listener removed everytime scroll stops.
-            window.removeEventListener("scroll");
+            window.removeEventListener("scroll", null);
         };
 
     }, []);
@@ -29,7 +29,15 @@ function Nav() {
        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
        alt= "Netflix Logo"
        />
-       
+
+        <div className="nav_selects">  {/* Best practice is with ul and li-tags, but for this mockup, this solution will suffice.*/}
+            <h5>Home</h5>
+            <h5>Series</h5>
+            <h5>Movies</h5>
+            <h5>New and popular</h5>
+            <h5>My list</h5>
+       </div>
+
        <img 
        className="nav_avatar"
        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
